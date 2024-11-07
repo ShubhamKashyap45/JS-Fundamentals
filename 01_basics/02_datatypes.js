e7528398df5a801c620dc3d2623e987ea0f6a232
Shubham Kashyap <60737510+ShubhamKashyap45@users.noreply.github.com>
2024-11-07 20:32:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+let integer = 5;
+let num = 10;
+let name = "Shubham";
+let bool = true;
+
+console.log(bool);
+console.log(typeof(bool));
+
+let valueInNumber = Number(num);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+let someNumber = String(integer);
+console.log(someNumber);
+console.log(typeof(someNumber));
+
+
+
+
+
